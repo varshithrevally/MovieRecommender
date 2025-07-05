201858b4898a4bfe15c06eb0d52391f73ea3c192
varshithrevally <revallyvarshith989@gmail.com>
2025-07-05 14:55:45 +0530

    Updated As Streamlit App

--- app.py
+++ app.py
@@ -1,67 +1,59 @@
-from flask import Flask, request, render_template
+import streamlit as st
 import pandas as pd
 import difflib
 import requests
+import os
+from dotenv import load_dotenv
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import urllib.parse
-from dotenv import load_dotenv
+
 load_dotenv()
+OMDB_API_KEY = os.getenv("OMDB_API_KEY")
 
-app=Flask(__name__)
-
-import os
-OMDB_API_KEY = os.environ.get("OMDB_API_KEY")
-
-movies_data=pd.read_csv('movies.csv')
-
-selected_features=['genres', 'keywords', 'tagline', 'cast', 'director']
+movies_data = pd.read_csv('movies.csv')
+selected_features = ['genres', 'keywords', 'tagline', 'cast', 'director']
 for feature in selected_features:
-    movies_data[feature]=movies_data[feature].fillna('')
-
-combined_features=movies_data['genres'] + ' ' + movies_data['keywords'] + ' ' + \
-                    movies_data['tagline'] + ' ' + movies_data['cast'] + ' ' + \
-                    movies_data['director']
-
-vectorizer=TfidfVectorizer()
-feature_vectors=vectorizer.fit_transform(combined_features)
-
-similarity=cosine_similarity(feature_vectors)
+    movies_data[feature] = movies_data[feature].fillna('')
+combined_features = movies_data['genres'] + ' ' + movies_data['keywords'] + ' ' + movies_data['tagline'] + ' ' + movies_data['cast'] + ' ' + movies_data['director']
+vectorizer = TfidfVectorizer()
+feature_vectors = vectorizer.fit_transform(combined_features)
+similarity = cosine_similarity(feature_vectors)
 
 def get_movie_poster(title):
-    query=urllib.parse.quote(title)
-    url=f"http://www.omdbapi.com/?t={query}&apikey={OMDB_API_KEY}"
-    response=requests.get(url)
-    data=response.json()
-    if data.get("Poster") and data["Poster"]!="N/A":
-        return data["Poster"]
-    else:
-        return "https://via.placeholder.com/300x450?text=No+Poster"
+    url = f"http://www.omdbapi.com/?t={title}&apikey={OMDB_API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    return data.get("Poster") if data.get("Poster") and data.get("Poster") != "N/A" else "https://via.placeholder.com/300x450?text=No+Poster"
 
 def recommend_movies(movie_name):
-    list_of_all_titles=movies_data['title'].tolist()
-    close_matches=difflib.get_close_matches(movie_name, list_of_all_titles)
-    if not close_matches:
+    list_of_all_titles = movies_data['title'].tolist()
+    find_close_match = difflib.get_close_matches(movie_name, list_of_all_titles)
+    if not find_close_match:
         return []
-    closest_match=close_matches[0]
-    index=movies_data[movies_data.title == closest_match].index.values[0]
-    similarity_scores=list(enumerate(similarity[index]))
-    sorted_similar_movies=sorted(similarity_scores, key=lambda x: x[1], reverse=True)
-    recommended_movies=[]
-    for movie in sorted_similar_movies[1:19]:
-        idx=movie[0]
-        title=movies_data.iloc[idx]['title']
-        poster_url=get_movie_poster(title)
-        recommended_movies.append({"title": title, "poster": poster_url})
+    close_match = find_close_match[0]
+    index_of_the_movie = movies_data[movies_data.title == close_match].index.values[0]
+    similarity_score = list(enumerate(similarity[index_of_the_movie]))
+    sorted_similar_movies = sorted(similarity_score, key=lambda x: x[1], reverse=True)
+    recommended_movies = []
+    for movie in sorted_similar_movies[1:11]:
+        index = movie[0]
+        title = movies_data.iloc[index]['title']
+        poster = get_movie_poster(title)
+        recommended_movies.append((title, poster))
     return recommended_movies
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    recommendations=[]
-    if request.method=='POST':
-        movie_name=request.form['movie_name']
-        recommendations=recommend_movies(movie_name)
-    return render_template('index.html', recommendations=recommendations)
+st.title("🎬 Movie Recommender System")
 
-if __name__=='__main__':
-    app.run(debug=True)
+movie_input = st.text_input("Enter your favorite movie")
+
+if st.button("Get Recommendations") and movie_input:
+    results = recommend_movies(movie_input)
+    if results:
+        for title, poster in results:
+            col1, col2 = st.columns([1, 2])
+            with col1:
+                st.image(poster, use_container_width=True)
+            with col2:
+                st.subheader(title)
+    else:
+        st.error("No recommendations found. Try another title.")
